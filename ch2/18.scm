58af3e4d5d79d2a7e31277bb8af40cecb9ad8187
Edward Banner <edward.banner@gmail.com>
2014-03-11 23:37:26 -0500

    Finish Exercise 18

---
+++ ch2/18.scm
@@ -0,0 +1,15 @@
+;; EXERCISE 2.18
+;: (reverse (list 1 4 9 16 25))
+
+(define (reverse l)
+  (define (reverse-iter l aggregate)
+    (if (null? l)
+        aggregate
+        (reverse-iter (cdr l) (cons (car l) aggregate))))
+  (reverse-iter l '()))
+
+(define (reverse l)
+  (if (null? l)
+      ()
+      (append (reverse (cdr l))
+              (list (car l)))))
