26c7a13f4e31075565998d9539b5a14292e7df04
Edward Banner <edward.banner@gmail.com>
2014-03-12 23:10:51 -0500

    Finish Exercise 25.scm

---
+++ ch2/25.scm
@@ -0,0 +1,11 @@
+;; EXERCISE 2.25
+
+;: (1 3 (5 7) 9)
+; (car (cdr (car (cdr (cdr x)))))
+
+;: ((7))
+; (car (car x))
+
+;: (1 (2 (3 (4 (5 (6 7))))))
+; (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr x))))))))))))
+
