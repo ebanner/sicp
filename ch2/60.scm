6749af799f208c63e43b45a151ac6136667e8868
Edward Banner <edward.banner@gmail.com>
2014-03-18 22:57:40 -0500

    Finish Exercise 60

---
+++ ch2/60.scm
@@ -0,0 +1,15 @@
+;; EXERCISE 2.60
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set) (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
