0e04ad1434b9190a8aa037aa0d3e9394754822aa
Edward Banner <edward.banner@gmail.com>
2014-03-19 23:32:47 -0500

    Finish Exercise 60

--- ch2/60.scm
+++ ch2/60.scm
@@ -13,3 +13,10 @@
          (cons (car set1)
                (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+; The efficiency for finding an element is better on average, although
+; still O(n). Unioning & Intersecting sets is can be better or
+; worse. If you're dealing with sets where you have put in a lot of
+; duplicates, they're going to suffer. However, if you have a set with
+; few duplicates, you may get lucky more often and have
+; element-of-set? catch an element earlier than it would have.
