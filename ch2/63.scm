7051380b9c8e7866fa53c5ccce19abdce39f350d
Edward Banner <edward.banner@gmail.com>
2014-03-19 23:34:33 -0500

    Finish Exercise 63

---
+++ ch2/63.scm
@@ -0,0 +1,26 @@
+;; EXERCISE 2.63
+
+; part a
+;
+; Yes, both procedures produce the exact same lists. They both produce
+; the list (1 3 5 7 9 11)
+
+; part b
+;
+; Yes, both procedures have growth O(n)
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
