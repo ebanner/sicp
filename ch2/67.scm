27c02e7be3372be51772acd5c8ab8a983be5f3b5
Edward Banner <edward.banner@gmail.com>
2014-03-20 23:14:58 -0500

    Finish Exercise 67.scm

---
+++ ch2/67.scm
@@ -0,0 +1,14 @@
+;; EXERCISE 2.67
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;; 1 ]=> (decode sample-message sample-tree)
+
+;; Value 14: (a d a b b c a)
