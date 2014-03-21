be014f909000f9be6a4fa323f734d143c86c1b8f
Edward Banner <edward.banner@gmail.com>
2014-03-20 23:15:45 -0500

    Finish Exercise 71.scm

---
+++ ch2/71.scm
@@ -0,0 +1,11 @@
+; EXERCISE 2.71
+
+; Since the sum of the lightest two trees are going to be juuuuuuuust
+; less (1 short) of the weight of the next node, the tree is built
+; ``sequentially" (Thx Eli Bendersky for this term).
+;
+; The bits needed to encode the most frequently occurring symbol in
+; such a bizarre frequency map is precisely n-1.
+;
+; The bits needed to encode the least frequently occurring symbol in
+; such a bizarre frequency map is precisely n-1.
