f07f15f1d626a3f0ce985aa0b32783ae090a61e0
Edward Banner <edward.banner@gmail.com>
2014-06-07 10:05:55 -0500

    Finish Exercise 82

---
+++ ch2/82.scm
@@ -0,0 +1,47 @@
+;;; Exercise 2.82
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (let ((types-all-others-can-be-coerced-to)
+		(filter (lambda (type) (can-be-coerced? type-tags
+							type))))
+	    (if (not (null? types-all-others-can-be-coerced-to))
+		(let ((type-all-others-can-be-coerced-to)
+		      (car types-all-others-can-be-coerced-to))
+		  (let ((coerced-objs)
+			(coerce-objs args
+				     type-all-thers-can-be-coerced-to))
+		    (apply-generic op coerced-objs)))
+		(error "Unable to coerce all args to a single type")))))))
+
+;;; Map each type to its coersion procedure to `type'. If any one of
+;;; these types are unable to be coerced to `type' then the following
+;;; accululation will result in #f.
+(define (can-be-coerced? types type)
+  (accumulate (map
+	       (lambda (t) (get-coersion t type))
+	       types)
+	      (lambda (boolean booleans) (and boolean booleans))
+	      #t))
+
+;;; Coerce each object by applying the coersion procedure to it
+(define (coerce-objs objs type)
+  (map (lambda (obj) ((get-coersion (type-tag obj) type) obj))
+       objs))
+
+;;; Accumulate
+(define (accumulate list proc nil)
+  (if (null? list)
+      nil
+      (proc (car list)
+	    (accumulate (cdr list) proc nil))))
+
+;;; Filter
+(define (filter list predicate?)
+  (cond ((null? list) '())
+	((predicate? (car list)) (cons (car list) (filter (cdr list)
+							  predicate?)))
+	(else (filter (cdr list) predicate?))))
