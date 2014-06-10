d16a10d5b65b95f7d15c2ad642cbb08515d92e9c
Edward Banner <edward.banner@gmail.com>
2014-06-09 22:34:52 -0500

    Finish Exercise 86

---
+++ ch2/86.scm
@@ -0,0 +1,11 @@
+;;; EXERCISE 2.86
+
+;;; I gave up on this one.
+;;;
+;;; But in order for this to work, one would need to implement generic
+;;; procedures for `sine', `cosine', `tangent', and `arctangent'. The
+;;; accessors `real-part', `imaginary-part', `angle', and `magnitude'
+;;; should use these generic procedures instead of the built-in scheme
+;;; ones. We also need to change the procedures `add-complex',
+;;; `sub-complex', `mul-complex', and `div-complex' to use the
+;;; corresponding generic procedures instead of the scheme built-in ones.
