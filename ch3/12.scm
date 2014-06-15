e0c3be6deaa17283be849274607f824529fe2380
Edward Banner <edward.banner@gmail.com>
2014-06-15 10:10:39 -0500

    Finish Exercise 3.12

---
+++ ch3/12.scm
@@ -0,0 +1,13 @@
+;;; EXERCISE 3.12
+
+
+;: (define x (list 'a 'b))
+;: (define y (list 'c 'd))
+;: (define z (append  x y))
+;: z
+;: (cdr x)
+;: (b)
+;: (define w (append! x y))
+;: w
+;: (cdr x)
+;: (b c d)
