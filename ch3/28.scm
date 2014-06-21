df059745a6a061421fc0604e3fcfdfb114e2b6bf
Edward Banner <edward.banner@gmail.com>
2014-06-21 15:12:08 -0500

    Finish Exercise 28

---
+++ ch3/28.scm
@@ -0,0 +1,14 @@
+;;; EXERICSE 3.28
+
+;;; This had to be the easiest exercise in the whole book. Just
+;;; globally replace `and' with `or' :)
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
