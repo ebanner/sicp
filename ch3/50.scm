00816d86a872709a41de78c21e824dc8eb071018
Edward Banner <edward.banner@gmail.com>
2014-06-22 11:27:48 -0500

    Finish Exercise 50

---
+++ ch3/50.scm
@@ -0,0 +1,9 @@
+;;; EXERCISE 3.50
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (stream-cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+	      (cons proc (map stream-cdr argstreams))))))
