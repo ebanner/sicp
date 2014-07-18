7492d73d94368fa0256c0dc482379a5c5106a416
Edward Banner <edward.banner@gmail.com>
2014-07-17 22:21:09 -0500

    Update Exercise 25

--- ch4/25.scm
+++ ch4/25.scm
@@ -5,3 +5,8 @@
 ;; the Scheme interpretor has to evaluate its arguments no matter what.
 ;; So (* n (factorial (- n 1))) is evaluated unconditionally, resulting
 ;; in an infinite recursive chain.
+;;
+;; Yes, this definition will work in a normal-order language. This is
+;; because the expression (* n (factorial (- n 1))) will only be
+;; evaluated if it's called upon. That is, when `n' is anything other
+;; than 1.
