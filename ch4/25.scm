4a0835ad519e703a1aafeac931e28055e8bb3d89
Edward Banner <edward.banner@gmail.com>
2014-07-17 22:18:55 -0500

    Finish Exercise 25

---
+++ ch4/25.scm
@@ -0,0 +1,7 @@
+;;; EXERCISE 4.25
+
+In good old applicative-order Scheme, `factorial' will never
+terminate. That's because in order to apply `unless' to its arguments,
+the Scheme interpretor has to evaluate its arguments no matter what.
+So (* n (factorial (- n 1))) is evaluated unconditionally, resulting
+in an infinite recursive chain.
