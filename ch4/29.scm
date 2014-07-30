0de48330fb4c82dd31842946da77e04c296562fd
Edward Banner <edward.banner@gmail.com>
2014-07-29 20:49:24 -0500

    * ch4/29.scm: Answer second part of question

--- ch4/29.scm
+++ ch4/29.scm
@@ -17,3 +17,30 @@
   (find-iter list))
 
 (find huge-list (exponential-fibonacci-procedure googolplex))
+
+;;; Using memoization
+;;; 
+(square (id 10))
+=> 100
+count
+=> 1
+
+;;; Without memoization
+;;;
+(square (id 10))
+=> 100
+count
+=> 2
+
+;;; `count' winds up having a value of 2 when memoization is not used
+;;; because `(id 10)' is captured as a thunk and evaluated twice in
+;;; the body of `square'. Since evaluating the body of `square'
+;;; involves incrementing `count', `count' is incremented twice.
+;;;
+;;; With memoization, the first time the `(id 10)' thunk is forced, it
+;;; increments `count', but caches the value of 10 as its result. So
+;;; when its forced again (as the second argument in the body of
+;;; `square'), its value is just 10.
+;;;
+;;; So memoizing thunks necessarily dictates any side effects in the
+;;; expression the thunk captured only run once.
